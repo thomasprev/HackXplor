53d574b9d9dcf392a32fb6cfcbb12370c9139c97
Joris Van Hecke <joris.vanhecke@gmail.com>
2013-12-06 08:24:56 +0100

    Bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    if params[:start]
+    if params[:page]
 
       @events = Event.order(:startdate).paginate(:page => params[:page], :per_page => 5)
       #raise @events
